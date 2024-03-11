48909fc548d33b6f893182e029ae9b75a57a4a03
tontay123 <123915278+tontay123@users.noreply.github.com>
2024-03-11 10:37:39 +0700

    supplier controller

---
+++ OracleGroupAssignment/Controllers/SupplierController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OracleGroupAssignment.Controllers
+{
+    public class SupplierController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
