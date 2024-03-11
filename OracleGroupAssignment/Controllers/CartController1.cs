76a710ce2c82f903860878f11c3e2d75c12c19cf
khunkimheng <155130387+khunkimheng@users.noreply.github.com>
2024-03-11 18:59:14 +0700

    cart commit

---
+++ OracleGroupAssignment/Controllers/CartController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OracleGroupAssignment.Controllers
+{
+    public class CartController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
